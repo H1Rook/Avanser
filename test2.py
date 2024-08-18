e8b85fb168528974bd5d33488b67cc2b1d619319
Rook <hiitsrook@gmail.com>
2024-08-18 17:42:57 +1000

    Done

--- test2.py
+++
@@ -1,8 +0,0 @@
-from common import execute_query, get_history_connection
-from ArchiveCdr import ArchiveCdr
-archiver = ArchiveCdr()
-
-sql = "show databases"
-
-dbs = archiver._query_history_db(sql)
-print(dbs)
